07c252a366ba9f58b947ef516da744e54c022329
pramodnovus <pramod.kumar@novusinsights.com>
2024-12-03 13:17:16 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-3xl font-bold text-red-500">
+      <h1>Namaste Everyone, let's build ERP</h1>
+    </div>
+  );
+}
+
+export default App;
